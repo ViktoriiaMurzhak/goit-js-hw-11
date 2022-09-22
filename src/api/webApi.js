92ffa133789ff497bcb531a770429370d2300e60
Viktoriia <viktoria.murzhak@gmail.com>
2022-09-22 17:12:02 +0300

    майже кінець

---
+++ src/api/webApi.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import { itemPerPage } from '../index';
+
+export const itemPerPage = 40;
+
+const API_KEY = '30087713-0176780a5d439b6fbf7b0e04e';
+const searchParams = new URLSearchParams({
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  per_page: itemPerPage,
+});
+
+export const BASE_URL = `https://pixabay.com/api/?${searchParams}`;
+
+export async function getPhoto(search, page) {
+  try {
+    if (!search.trim()) {
+      console.log('no arg!');
+      return;
+    }
+    const response = await axios.get(`${BASE_URL}&page=${page}&q=${search}`);
+    return response.data;
+  } catch (error) {
+    Notiflix.Notify.failure(error.message);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,89 @@
 import './css/styles.css';
+import { BASE_URL, getPhoto, itemPerPage } from './api/webApi';
+import Notiflix from 'notiflix';
+
+const galleryEl = document.querySelector('.gallery');
+const formEl = document.querySelector('#search-form');
+const moreBtn = document.querySelector('.load-more');
+
+let page = 1;
+
+const totalPages = Math.ceil(500 / itemPerPage);
+
+formEl.addEventListener('submit', onSubmit);
+
+async function loadMoreCards(searchValue) {
+  page += 1;
+  const data = await getPhoto(searchValue, page);
+  data.hits.forEach(photo => {
+    createCardMarkup(photo);
+  });
+  if (page === totalPages) {
+    moreBtn.classList.add('visually-hidden');
+  }
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  clearMarkup(galleryEl);
+
+  const searchValue = event.currentTarget[0].value;
+  mountData(searchValue);
+}
+
+async function mountData(searchValue) {
+  try {
+    const data = await getPhoto(searchValue, page);
+
+    moreBtn.classList.remove('visually-hidden');
+    moreBtn.addEventListener('click', () => {
+      loadMoreCards(searchValue);
+    });
+    if (data.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    data.hits.forEach(photo => {
+      createCardMarkup(photo);
+    });
+  } catch (error) {
+    console.log('error', error);
+  }
+}
+
+function createCardMarkup({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  galleryEl.insertAdjacentHTML(
+    'beforeend',
+    `<div class="photo-card">
+  <img src=${webformatURL} alt=${tags} loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes:</b><span>${likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`
+  );
+}
+
+function clearMarkup(element) {
+  element.innerHTML = '';
+}
